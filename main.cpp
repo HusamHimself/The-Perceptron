e936ac0415fb32985b58ac97ea52d3765a55e422
husamhimself <tahirihusam@gmail.com>
2026-09-22 23:08:14 -0400

    contributed to making the logic complete and added a more clear picture of what will the finished project look like.

--- main.cpp
+++ main.cpp
@@ -3,18 +3,45 @@
 //Thanks to welsh labs for writig a fascinating book.
 
 void displayBoard(bool board[4][4]);
-
+double calculateTotal(bool switches[4][4], double weights[4][4]);
+double learnFrom(bool switches[4][4], double weights[4][4], double learning_rate);
 
 int main(){
-    double learning_rate = 0;
+    double learning_rate = 0.01;
     bool switches[4][4] =  {{false, false, false, false},
                             {false, false, false, false},
                             {false, false, false, false},
                             {false, false, false, false}};
-
+    double weights[4][4] = {{0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0}};
+    
     displayBoard(switches);
 
-    
+}
+
+double learnFrom(bool switches[4][4], double weights[4][4], double learning_rate){
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if(switches[i][j] == true){
+                weights[i][j] += learning_rate;
+                continue;
+            }
+            weights[i][j] -= learning_rate;
+        }
+    }
+}
+
+double calculateTotal(bool switches[4][4], double weights[4][4]){
+    double theSum = 0;
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if(switches[i][j] == true){
+                theSum += switches[i][j];
+            }
+        }
+    }
 }
 
 void displayBoard(bool board[4][4]){
